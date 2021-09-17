aed0b7bc2a98cb2e0aede91e7f03d5028575fa47
multicampus <plain3785@gmail.com>
2021-09-17 11:17:43 +0900

    [FE] feat: Add skeleton code (S05P21B205-149)

---
+++ frontend/src/styles/index.js
@@ -0,0 +1,19 @@
+export const colors = {
+  green: "#00B894",
+  yellow: "FDCB6E",
+  black: "#2D3436",
+  darkGray: "#636E72",
+  gray: "#f6f8fa",
+  lightGray: "#f6f8fa",
+  red: "#FF7675",
+  white: "#fff",
+};
+
+export const fonts = {
+  xxl: "20px",
+  xl: "18px",
+  lg: "16px",
+  md: "14px",
+  sm: "12px",
+  xs: "10px",
+};
