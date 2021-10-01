6359645c219cf08ea9a383465aead0a00b0f4418
multicampus <plain3785@gmail.com>
2021-10-01 15:01:27 +0900

    [FE] feat: Add footerNav component (S05P21B205-196)
    Former-commit-id: f6a5df56a6432cde9bd86293bbc8d462200682fc

---
+++ frontend/src/components/molecules/FooterNav/index.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { Wrapper, IconBox } from "./styles";
+import { Icon } from "@iconify/react";
+
+function FooterNav({ isActive = 0 }) {
+  return (
+    <>
+      <Wrapper>
+        <Link to="/">
+          <IconBox>
+            <Icon
+              icon="ic:round-health-and-safety"
+              className={`icon ${isActive === 0 ? "active" : ""}`}
+            />
+            <span>의약품</span>
+          </IconBox>
+        </Link>
+        <Link to="/">
+          <IconBox>
+            <Icon
+              icon="carbon:pills-subtract"
+              className={`icon ${isActive === 1 ? "active" : ""}`}
+            />
+            <span>복용주의</span>
+          </IconBox>
+        </Link>
+        <Link to="/">
+          <IconBox>
+            <Icon
+              icon="healthicons:health-data-security"
+              className={`icon ${isActive === 2 ? "active" : ""}`}
+            />
+            <span>나의 약</span>
+          </IconBox>
+        </Link>
+        <Link to="/mypage">
+          <IconBox>
+            <Icon
+              icon="healthicons:ui-user-profile-outline"
+              className={`icon ${isActive === 3 ? "active" : ""}`}
+            />
+            <span>마이페이지</span>
+          </IconBox>
+        </Link>
+      </Wrapper>
+    </>
+  );
+}
+
+export default FooterNav;
