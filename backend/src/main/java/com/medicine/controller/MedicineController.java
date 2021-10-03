d33e4392a562d8be7610f20c06af9dff9d2e3330
Shimyeeun <yeeun7873@naver.com>
2021-10-03 14:38:09 +0900

    [BE] feat: Add get MedicineDetailInfo (S05P21B205-223)
    Former-commit-id: b2e0ba63221088860557b62f26f1d69859dc1873

--- backend/src/main/java/com/medicine/controller/MedicineController.java
+++ backend/src/main/java/com/medicine/controller/MedicineController.java
@@ -1,9 +1,12 @@
 package com.medicine.controller;
 
+import com.medicine.dto.medicine.DetailOutput;
 import com.medicine.dto.medicine.SimilarInput;
 import com.medicine.dto.medicine.SimilarOutput;
 import com.medicine.response.PageResponse;
+import com.medicine.response.Response;
 import com.medicine.service.MedicineService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +30,18 @@ public class MedicineController {
     public PageResponse<SimilarOutput> signUp(@PathVariable("id") String id, @Valid SimilarInput similarInput) {
         log.info("[GET] /medicines/similar/" + id);
         return medicineService.getSimilarMedicineInfo(id, similarInput);
+    }
+
+    /**
+     * 약 상세정보 조회 API
+     * [GET] /medicines/{id}
+     * @return Response<DetailOutput>
+     */
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "약 상세정보 조회", notes = "약의 상세정보를 조회한다.")
+    public Response<DetailOutput> getDetailMedicineInfo(@PathVariable("id") String id){
+        log.info("[GET] /medicines/" + id);
+        return medicineService.getDetailMedicineInfo(id);
     }
 }
